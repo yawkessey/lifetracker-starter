9e0ea24f28781ccc980e6e5b37c120b801cbef42
Yaw Kessey-Ankomah <yawkesseyankomah@gmail.com>
2022-07-07 23:28:09 -0700

    Landing Page + Navbar Complete

---
+++ lifetracker-starter-app-ui/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import Landing from './Landing/Landing';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
